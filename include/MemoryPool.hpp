3dd2b59845a816eb396222fc41f16b33ba3d4669
chintan-27 <chintan.acharya27@gmail.com>
2025-05-28 19:23:43 -0400

    Adds Memory Pool which uses bump allocator as backend

---
+++ include/MemoryPool.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "BumpAllocator.hpp"
+#include "Logger.hpp"
+#include <vector>
+#include <functional>
+
+template <typename T>
+class MemoryPool {
+public:
+    MemoryPool(BumpAllocator& allocator, std::size_t capacity)
+        : allocator(allocator), capacity(capacity) {
+        Logger::log("[MemoryPool] Created pool for type '" + std::string(typeid(T).name()) + "' with capacity " + std::to_string(capacity));
+    }
+
+    template <typename... Args>
+    T* make(Args&&... args) {
+        if (count >= capacity) {
+            Logger::log("[MemoryPool] Out of capacity for type '" + std::string(typeid(T).name()) + "'");
+            throw std::bad_alloc();
+        }
+
+        T* obj = allocator.make<T>(std::forward<Args>(args)...);
+        ++count;
+        Logger::log("[MemoryPool] Allocated object of type '" + std::string(typeid(T).name()) + "'");
+        return obj;
+    }
+
+    std::size_t size() const { return count; }
+    std::size_t max_size() const { return capacity; }
+
+private:
+    BumpAllocator& allocator;
+    std::size_t capacity;
+    std::size_t count = 0;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,92 @@
 #include "../include/BumpAllocator.hpp"
+#include "../include/Logger.hpp"
+#include "../include/MemoryPool.hpp"  //
 #include <iostream>
+#include <string>
 
 struct Foo {
     int x;
     Foo(int val) : x(val) {
-        std::cout << "Foo constructed with " << x << "\n";
+        Logger::log("Foo constructed with " + std::to_string(x));
     }
     ~Foo() {
-        std::cout << "Foo destructed with " << x << "\n";
+        Logger::log("Foo destructed with " + std::to_string(x));
     }
 };
 
-int main(int argc, char** argv) {
+struct TrackedObject {
+    int value;
+
+    TrackedObject(int v) : value(v) {
+        Logger::log("TrackedObject constructed with value " + std::to_string(value));
+    }
+
+    ~TrackedObject() {
+        Logger::log("TrackedObject destructed with value " + std::to_string(value));
+    }
+
+    static BumpAllocator* allocator;
+
+    void* operator new(std::size_t size) {
+        Logger::log("[Custom New] Allocating " + std::to_string(size) + " bytes for TrackedObject");
+        return allocator->allocate(size);
+    }
+
+    void operator delete(void* ptr) {
+        Logger::log("[Custom Delete] Ignored (BumpAllocator doesn't support free)");
+    }
+};
+
+BumpAllocator* TrackedObject::allocator = nullptr;
+
+int main(int argc, char* argv[]) {
     constexpr std::size_t SIZE = 1024;
     alignas(std::max_align_t) char buffer[SIZE];
     BumpAllocator allocator(buffer, SIZE);
+    TrackedObject::allocator = &allocator;
 
-    // Parse flags first
-    bool enableLog = false;
-    bool showStatus = false;
-    bool showDump = false;
-
+    // CLI flag processing
     for (int i = 1; i < argc; ++i) {
         std::string flag = argv[i];
-        if (flag == "--log") Logger::enabled = true;
-        else if (flag == "--nolog") Logger::enabled = false;
-        else if (flag == "--status") showStatus = true;
-        else if (flag == "--dump") showDump = true;
+        if (flag == "--log") {
+            Logger::enabled = true;
+        } else if (flag == "--nolog") {
+            Logger::enabled = false;
+        }
     }
 
-    // Actual usage
-    int* a = allocator.allocate<int>("int a");
+    // allocate<T>
+    int* a = allocator.allocate<int>();
     *a = 42;
+    Logger::log("Allocated int: " + std::to_string(*a));
 
+    // make<T>
     Foo* f = allocator.make<Foo>(99);
+    Logger::log("Foo object at: " + std::to_string(reinterpret_cast<std::uintptr_t>(f)));
 
-    if (showStatus) {
-        std::cout << "[Allocator Status] Used: " << allocator.getUsedBytes()
-                  << ", Remaining: " << allocator.getRemainingBytes() << "\n";
+    // operator new overload
+    TrackedObject* obj = new TrackedObject(123);
+    Logger::log("TrackedObject value: " + std::to_string(obj->value));
+
+    // MemoryPool usage
+    MemoryPool<Foo> fooPool(allocator, 5);
+    Foo* pooled1 = fooPool.make(111);
+    Foo* pooled2 = fooPool.make(222);
+
+    Logger::log("MemoryPool created " + std::to_string(fooPool.size()) + " objects");
+
+    // Post-allocation CLI commands
+    for (int i = 1; i < argc; ++i) {
+        std::string flag = argv[i];
+        if (flag == "--status") {
+            std::cout << "[Allocator Status] Used: " << allocator.getUsedBytes()
+                      << ", Remaining: " << allocator.getRemainingBytes() << "\n";
+        }
+        if (flag == "--dump") {
+            allocator.dumpMemory();
+        }
     }
 
-    if (showDump) {
-        allocator.dumpMemory();
-    }
-
-    std::cout << "Done\n";
     allocator.reset();
     return 0;
 }
-
